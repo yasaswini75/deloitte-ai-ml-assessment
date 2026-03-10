2ce1a965588f5a8becdb6f332397546839b23527
yasaswini75 <100273318+yasaswini75@users.noreply.github.com>
2026-03-10 20:47:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from skill_extractor import extract_skills
+from job_matcher import calculate_match
 
+required_skills = ["python","sql","machine learning","data analysis"]
+
+with open("sample_resume.txt","r") as file:
+    resume = file.read()
+
+candidate_skills = extract_skills(resume)
+
+score, matched = calculate_match(candidate_skills, required_skills)
+
+print("Detected Skills:", candidate_skills)
+print("Matched Skills:", matched)
+print("Job Match Score:", score,"%")
