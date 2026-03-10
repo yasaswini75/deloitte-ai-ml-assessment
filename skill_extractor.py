c0fce2d28727b5af562b827936713c42cb813d58
yasaswini75 <100273318+yasaswini75@users.noreply.github.com>
2026-03-10 20:45:45 +0530

    Update skill_extractor.py

--- skill_extractor.py
+++ skill_extractor.py
@@ -1,1 +1,13 @@
+skills_db = ["python","sql","machine learning","pandas","numpy","data analysis"]
 
+def extract_skills(resume_text):
+
+    resume_text = resume_text.lower()
+
+    found_skills = []
+
+    for skill in skills_db:
+        if skill in resume_text:
+            found_skills.append(skill)
+
+    return found_skills
